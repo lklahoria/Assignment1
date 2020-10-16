242749c65a2709073b62f275ea266b307cd6187f
llalit <erlalit.lahoria@gmail.com>
2020-10-16 10:35:17 +0530

    Updates

---
+++ precache-manifest.dd8609dd7a228eb343f50af53c427d1b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57198357e1694082508cf4a208cc6807",
+    "url": "/Assignment1/index.html"
+  },
+  {
+    "revision": "93c296f5b1b1052883ae",
+    "url": "/Assignment1/static/css/2.f12ece30.chunk.css"
+  },
+  {
+    "revision": "fba73996353cc8ad70fa",
+    "url": "/Assignment1/static/css/main.a388d54b.chunk.css"
+  },
+  {
+    "revision": "93c296f5b1b1052883ae",
+    "url": "/Assignment1/static/js/2.9716fa61.chunk.js"
+  },
+  {
+    "revision": "7d0b5c1cc1715c8844e100015674cba1",
+    "url": "/Assignment1/static/js/2.9716fa61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba73996353cc8ad70fa",
+    "url": "/Assignment1/static/js/main.cb7fbdcd.chunk.js"
+  },
+  {
+    "revision": "d09db5b3f6eaeae15223",
+    "url": "/Assignment1/static/js/runtime-main.e010560e.js"
+  },
+  {
+    "revision": "27828a4ffb56b8d0a40338fd459d263b",
+    "url": "/Assignment1/static/media/Bangalore.27828a4f.png"
+  },
+  {
+    "revision": "7c05118b7158a825a979ab7697057b93",
+    "url": "/Assignment1/static/media/BusinessCategoriesbg.7c05118b.jpg"
+  },
+  {
+    "revision": "0801a12a537c90638adff261adaf2b66",
+    "url": "/Assignment1/static/media/advicefeatures1.0801a12a.png"
+  },
+  {
+    "revision": "d873a012593c59632fc1bda37a41a6fe",
+    "url": "/Assignment1/static/media/chennai.d873a012.png"
+  },
+  {
+    "revision": "ab3526d3adc09a98df25648e5927b6c7",
+    "url": "/Assignment1/static/media/hotel.ab3526d3.png"
+  },
+  {
+    "revision": "7db45e985c13f595dc1068be9ccfc890",
+    "url": "/Assignment1/static/media/logo.7db45e98.png"
+  },
+  {
+    "revision": "6969631017959d5cd9edd74e408dcdd3",
+    "url": "/Assignment1/static/media/logo2.69696310.png"
+  },
+  {
+    "revision": "860500ca69479349faa4562c63428168",
+    "url": "/Assignment1/static/media/manufacturing.860500ca.png"
+  },
+  {
+    "revision": "656a853efd8ab95bca4689ede3990f86",
+    "url": "/Assignment1/static/media/mumbai.656a853e.png"
+  },
+  {
+    "revision": "c4e17d3e96374354c6b6a1a91e8d250e",
+    "url": "/Assignment1/static/media/restaurants.c4e17d3e.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Assignment1/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Assignment1/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Assignment1/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Assignment1/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6da0f80d51c5ae017d578425a17bd73f",
+    "url": "/Assignment1/static/media/sliderimg.6da0f80d.jpg"
+  },
+  {
+    "revision": "a80234a95fe9788c80a21a8a2530c389",
+    "url": "/Assignment1/static/media/tamilnadu.a80234a9.png"
+  },
+  {
+    "revision": "632721517cb990073ac17d7f86cd3127",
+    "url": "/Assignment1/static/media/timethesale.63272151.png"
+  },
+  {
+    "revision": "e1b251ef631c2e1c50170f3b2339e027",
+    "url": "/Assignment1/static/media/usingabroker.e1b251ef.png"
+  },
+  {
+    "revision": "a61d8e46f9ff1a1bf5e253b4aba2aeef",
+    "url": "/Assignment1/static/media/usingabroker1.a61d8e46.png"
+  },
+  {
+    "revision": "25af4639e3f2431ee4512701727637ae",
+    "url": "/Assignment1/static/media/website.25af4639.png"
+  }
+]);
